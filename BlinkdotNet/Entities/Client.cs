4e19f7e6a7e7c3e939def92bbcef2c6a11bb3b01
Ernest Mallet <ernest.mallett@localmed.com>
2018-12-12 12:35:25 -0600

    Device and client cleanup

--- BlinkdotNet/Entities/Client.cs
+++ BlinkdotNet/Entities/Client.cs
@@ -1,30 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace BlinkdotNet.Entities
 {
+    [DataContract]
     public class Client
     {
-        public int id { get; set; }
-        public string created_at { get; set; }
-        public string updated_at { get; set; }
-        public string name { get; set; }
-        public string notify_sound { get; set; }
-        public string client_type { get; set; }
-        public string client_specifier { get; set; }
-        public string notification_key { get; set; }
-        public string aws_key { get; set; }
-        public string os_version { get; set; }
-        public string device_identifier { get; set; }
-        public string app_version { get; set; }
-        public string unique_id { get; set; }
-        public string home { get; set; }
-        public string ssid { get; set; }
-        public string ssid_updated_at { get; set; }
-        public int enabled_notifications { get; set; }
-        public string client_options { get; set; }
-        public int user_id { get; set; }
-        public int account_id { get; set; }
+        [DataMember(Name ="id")]
+        public int Id { get; set; }
+
+        [DataMember(Name ="created_at")]
+        public DateTime Created { get; set; }
+
+        [DataMember(Name ="updated_at")]
+        public DateTime Updated { get; set; }
+
+        [DataMember(Name ="name")]
+        public string Name { get; set; }
+
+        [DataMember(Name ="notify_sound")]
+        public bool NotifySound { get; set; }
+
+        [DataMember(Name = "client_type")]
+        public string ClientType { get; set; }
+
+        [DataMember(Name ="client_specifier")]
+        public string ClientSpecifier { get; set; }
+
+        [DataMember(Name ="notification_key")]
+        public string NotificationKey { get; set; }
+
+        [DataMember(Name ="aws_key")]
+        public string AwsKey { get; set; }
+
+        [DataMember(Name ="os_version")]
+        public string OsVersion { get; set; }
+
+        [DataMember(Name ="device_identifier")]
+        public string DeviceIdentifier { get; set; }
+
+        [DataMember(Name ="app_version")]
+        public string AppVersion { get; set; }
+
+        [DataMember(Name ="unique_id")]
+        public string UniqueId { get; set; }
+
+        [DataMember(Name ="home")]
+        public string Home { get; set; }
+
+        [DataMember(Name ="ssid")]
+        public string SSID { get; set; }
+
+        [DataMember(Name ="ssid_updated_at")]
+        public DateTime SSIDUpdated { get; set; }
+
+        [DataMember(Name = "enabled_notifications")]
+        public bool EnabledNotifications { get; set; }
+
+        [DataMember(Name ="client_options")]
+        public string ClientOptions { get; set; }
+
+        [DataMember(Name ="user_id")]
+        public int UserId { get; set; }
+
+        [DataMember(Name ="account_id")]
+        public int AccountId { get; set; }
     }
 }
--- BlinkdotNet/Entities/Device.cs
+++ BlinkdotNet/Entities/Device.cs
@@ -1,29 +1,72 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace BlinkdotNet.Entities
 {
+    [DataContract]
     public class Device
     {
-        public string device_type { get; set; }
-        public int device_id { get; set; }
-        public string type { get; set; }
-        public DateTime updated_at { get; set; }
-        public string name { get; set; }
-        public string thumbnail { get; set; }
-        public string active { get; set; }
-        public int notifications { get; set; }
-        public int warning { get; set; }
-        public string error_msg { get; set; }
-        public string status { get; set; }
-        public bool enabled { get; set; }
-        public bool armed { get; set; }
-        public int errors { get; set; }
-        public int wifi_strength { get; set; }
-        public int lfr_strength { get; set; }
-        public int temp { get; set; }
-        public int battery { get; set; }
-        public string battery_state { get; set; }
-        public bool usage_rate { get; set; }
-        public DateTime? last_hb { get; set; }
+        [DataMember(Name ="device_type")]
+        public string DeviceType { get; set; }
+
+        [DataMember(Name ="device_id")]
+        public int DeviceId { get; set; }
+
+        [DataMember(Name ="type")]
+        public string Type { get; set; }
+
+        [DataMember(Name ="updated_at")]
+        public DateTime Updated { get; set; }
+
+        [DataMember(Name ="name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "thumbnail")]
+        public string Thumbnail { get; set; }
+
+        [DataMember(Name ="active")]
+        public bool Active { get; set; }
+
+        [DataMember(Name ="notifications")]
+        public int Notifications { get; set; }
+
+        [DataMember(Name ="warning")]
+        public int Warning { get; set; }
+
+        [DataMember(Name ="error_msg")]
+        public string ErrorMessage { get; set; }
+
+        [DataMember(Name ="status")]
+        public string Status { get; set; }
+
+        [DataMember(Name ="enabled")]
+        public bool Enabled { get; set; }
+
+        [DataMember(Name ="armed")]
+        public bool Armed { get; set; }
+
+        [DataMember(Name ="errors")]
+        public int Errors { get; set; }
+
+        [DataMember(Name = "wifi_strength")]
+        public int WifiStrength { get; set; }
+
+        [DataMember(Name ="lfr_strength")]
+        public int LfrStrength { get; set; }
+
+        [DataMember(Name ="temp")]
+        public int Temperature { get; set; }
+
+        [DataMember(Name ="battery")]
+        public int Battery { get; set; }
+
+        [DataMember(Name ="battery_state")]
+        public string BatteryState { get; set; }
+
+        [DataMember(Name ="usage_rate")]
+        public bool UseageRate { get; set; }
+
+        [DataMember(Name ="last_hb")]
+        public DateTime? LastHb { get; set; }
     }
 }
